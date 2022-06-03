01c067735ded30896680d90b96397dd5d0935694
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-06-03 11:11:50 -0400

    Route to create a new liked character, or update like count for exist character complete.

--- backend/routes/characters.js
+++ backend/routes/characters.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const { User } = require('../models/user');
 const { Character, validateCharacter } = require('../models/character');
-
 const auth = require('../middleware/auth');
 
 
@@ -17,7 +14,7 @@ router.get('/', async (req, res) => {
             return res
                 .status(400)
                 .send('No characters in this collection.');
-        
+
         characters.map(like => {
             characterData.push([
                 like.marvelName,
@@ -36,7 +33,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-// UPDATE a character like count, including text and likes.
+// POST to create a new character with like counter, or update an exist character's like count.
 router.post("/characterLike/:marvelID", [auth], async (req, res) => {
     try {
         const { error } = validateCharacter(req.body);
@@ -45,70 +42,22 @@ router.post("/characterLike/:marvelID", [auth], async (req, res) => {
                 .status(400)
                 .send(`Body for character not valid! ${error}`);
 
-        let newCharacter = new Character(req.body);
-        await newCharacter.save();
+        let character = await Character.find({ marvelID: { $in: req.params.marvelID } });
+
+        console.log('req.params.marvelID: ', req.params.marvelID);
+        console.log('character found: ', character);
+        if (character.length == 0) {
+            character = new Character(req.body);
+            await character.save();
+        } else if (character.length == 1) {
+            console.log('should add 1 to likes.', character[0].likes);
+            character[0].likes = character[0].likes + 1;
+            await character[0].save();
+        }
 
         return res
             .status(200)
-            .send(newCharacter);
-    } catch (error) {
-        return res
-            .status(500)
-            .send(`Internal Server Error: ${error}`);
-    }
-});
-
-// UPDATE a character like count, including text and likes.
-router.post("/:userID/updatePost/:postID", [auth], async (req, res) => {
-    try {
-        const { error } = validatePost(req.body);
-        if (error)
-            return res
-                .status(400)
-                .send(`Body for post not valid! ${error}`);
-
-        let user = await User.findById(req.params.userID);
-        if (!user)
-            return res
-                .status(400)
-                .send(`User with ObjectId ${req.params.userID} does not exist.`);
-
-        let post = await Post.findById(req.params.postID);
-        if (!post)
-            return res
-                .status(400)
-                .send(`Post with Objectid ${req.params.postID} does not exist.`);
-
-        post.text = req.body.text;
-        post.likes = req.body.likes;
-        await post.save();
-
-        return res
-            .status(200)
-            .send(post);
-    } catch (error) {
-        return res
-            .status(500)
-            .send(`Internal Server Error: ${error}`);
-    }
-});
-
-// DELETE a user post by postID.
-router.delete("/:userID/deletePost/:postID", [auth], async (req, res) => {
-    try {
-        let user = await User.findById(req.params.userID);
-        if (!user)
-            return res
-                .status(400)
-                .send(`User with ObjectId ${req.params.userID} does not exist.`);
-
-        user.posts.id(req.params.postID).remove();
-
-        await user.save();
-
-        return res
-            .status(200)
-            .send(user);
+            .send(character);
     } catch (error) {
         return res
             .status(500)
